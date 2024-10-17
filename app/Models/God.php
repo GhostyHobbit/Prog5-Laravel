43aafd2dc62ceda5e2931e109d92f721805e1b12
Lucas <sennavdkoppel@gmail.com>
2024-10-17 10:27:00 +0200

    linked 1-to-many between users and gods

--- app/Models/God.php
+++ app/Models/God.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class God extends Model
-{
-
+class God extends Model {
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/gods-detail.blade.php
+++ resources/views/gods-detail.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
     <h1>{{$god->name}}</h1>
+    <p>Worshipped by {{ $god->user->name }}</p>
 </x-layout>
